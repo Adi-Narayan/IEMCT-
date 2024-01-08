a5ec0a18b7787a3821e5d99ed3a79e5d4c8805a7
Adi <90101489+Adi-Narayan@users.noreply.github.com>
2024-01-09 01:55:12 +0530

    Update question1.py

--- question1.py
+++ question1.py
@@ -1,16 +1,9 @@
-print("Input a linear equation with 3 variables")
 const = int(input('const = '))
 print(f'Given equation is: x + y + z = {const}')
 
-for x in range(++const):
-    z = 0
-    y = 0
+for x in range(++const): 
     for y in range(++const):
-        z = 0
         for z in range(++const):
             if (x + y + z == const):
                print(f'x = {x}, y = {y}, z = {z}')
-               break
-            z += 1
-        y += 1    
-    x += 1  
+               break 
